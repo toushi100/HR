88f66cb7494ccf75bdfcd3b9ed2d8842aca5c2ce
toushi100 <toushi2009@gmail.com>
2022-02-02 12:36:29 +0200

    added validations  and model associations to teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,10 @@
 class Team < ApplicationRecord
+    validates! :name, length: { minimum: 2 ,maximum: 15} ,presence: true
+    validates! :description, length: { minimum: 20} ,presence: true
+    validates! :members ,presence: true
+    validates! :team_lead ,presence: true
+
+    belongs_to :Division
+    has_many :Employees
+    has_many :Targets
 end
