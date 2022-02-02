84ab36d5aa5844b0dba1e7fef07f744b0f1026c0
toushi100 <toushi2009@gmail.com>
2022-02-02 12:50:13 +0200

    added validations  and model associations to division  model

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,2 +1,10 @@
 class Division < ApplicationRecord
+    validates! :name, length: { minimum: 2 ,maximum: 15} ,presence: true
+    validates! :email,:uniqueness => {:case_sensitive => false} ,presence: true
+    validates! :job ,presence: true
+    validates! :salary ,presence: true , inclusion: { in: 0..9 },numericality: true
+    validates! :employment_status ,presence: true
+
+    belongs_to :Division
+    belongs_to :Team
 end
