16e65495dc9828e264d92c6ca7e1677ed673b584
toushi100 <toushi2009@gmail.com>
2022-02-02 12:29:03 +0200

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates! :name, length: { minimum: 2 ,maximum: 15} ,presence: true
+    validates! :email,:uniqueness => {:case_sensitive => false} ,presence: true
+    validates! :passward,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },presence: true
+
+
 end
+
+# user = User.new
+# user.name = "Ahmed"
+# user.name = "Ali"
+# user.save
