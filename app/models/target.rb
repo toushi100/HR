db54890ac90a246c016c2af9e1dc331fe1b536a2
toushi100 <toushi2009@gmail.com>
2022-02-02 13:06:37 +0200

    fixed start_date and finish_date validator target  model

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,10 +2,19 @@ class Target < ApplicationRecord
     validates! :title, length: { minimum: 2 ,maximum: 15} ,presence: true
     validates! :description, length: { minimum: 20} ,presence: true
     validates! :start_date ,presence: true
-    validates! :finish_date , presence: true, date: { after_or_equal_to:  :start_date}
+    validates! :finish_date , presence: true
     validates! :team ,presence: true
     validates! :status ,presence: true
 
 
     belongs_to :Team
+
+    def date_validation
+        if self[:end_date] < self[:start_date]
+          errors[:end_date] << "Error message"
+          return false
+        else
+          return true
+        end
+      end
 end
