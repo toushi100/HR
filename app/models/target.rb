4af9bdc912e7cff4e23757e2a3f185e5028cc400
toushi100 <toushi2009@gmail.com>
2022-02-02 12:43:14 +0200

    added validations  and model associations to Target  model

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,2 +1,11 @@
 class Target < ApplicationRecord
+    validates! :title, length: { minimum: 2 ,maximum: 15} ,presence: true
+    validates! :description, length: { minimum: 20} ,presence: true
+    validates! :start_date ,presence: true
+    validates! :finish_date , presence: true, date: { after_or_equal_to:  :start_date}
+    validates! :team ,presence: true
+    validates! :status ,presence: true
+
+
+    belongs_to :Team
 end
