dfb83f00538f20a476c50c39bbf2427feb76cc2f
toushi100 <toushi2009@gmail.com>
2022-02-05 17:44:23 +0200

    added create new view,controller and routes to employee

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -11,4 +11,31 @@ class EmployeeController < ApplicationController
   
   end
 
+  def new
+    @employee = Employee.new
+  end
+
+  def create  
+    @employee = Employee.new#(post_params)
+    p "---------------------------------CREATE------------------------------"
+    @employee.name = params[:name]
+     @employee.email = params[:email]
+     @employee.job = params[:job]
+     @employee.salary = params[:salary]
+     @employee.employee_status = params[:employee_status]
+     @employee.save
+    p "---------------------------------CREATED------DONE------------------------"
+
+   respond_to do |format|
+     if @employee.save
+       format.html { redirect_to user_url(@employee), notice: "user was successfully created." }
+       format.json { render :show, status: :created, location: @employee }
+     else
+       format.html { render :new, status: :unprocessable_entity }
+       format.json { render json: @employee.errors, status: :unprocessable_entity }
+       
+     end
+   end
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,15 @@ Rails.application.routes.draw do
   delete '/user/:id' ,to:"user#destroy"
   delete '/employee/:id' ,to:"employee#destroy"
   get 'user/new', to: 'user#new', as: 'new_user' # new
+  get 'employee/new', to: 'employee#new' # new
+  get 'devision/new', to: 'division#new' # new
+  get 'teams/new', to: 'team#new' # new
+
+
   post 'user/create', to: 'user#create' # new
   post 'employee/create', to: 'employee#create' # new
+  post 'devision/create', to: 'devision#create' # new
+  post 'teams/create', to: 'teams#create' # new
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
