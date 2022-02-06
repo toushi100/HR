cbab01654a1bbb1d885931b1289bf85f40d16891
toushi100 <toushi2009@gmail.com>
2022-02-06 14:35:47 +0200

    added show view and functionality to division

--- app/controllers/division_controller.rb
+++ app/controllers/division_controller.rb
@@ -2,4 +2,41 @@ class DivisionController < ApplicationController
   def index
     @div = Division.all
   end
+
+  def new
+    @div = Division.new
+  end
+  def show 
+    @div = Division.find(params[:id])
+    end
+
+  def destroy
+    @div = Division.find(params[:id])
+    @div.destroy
+    redirect_to division_path
+  end
+
+  def create  
+    @div = Division.new#(post_params)
+    p "---------------------------------CREATE------------------------------"
+    @div.name = params[:name]
+    @div.description = params[:description]
+    @div.teams = params[:teams]
+    @div.manager = params[:manager]
+    @div.save
+    p "---------------------------------CREATED------DONE------------------------"
+
+    respond_to do |format|
+      if @div.save
+        format.html { redirect_to division_path, notice: "user was successfully created." }
+        format.json { render :show, status: :created, location: @div }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @div.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
 end
--- app/models/division.rb
+++ app/models/division.rb
@@ -1,9 +1,9 @@
 class Division < ApplicationRecord
     validates! :name, length: { minimum: 2 ,maximum: 15} ,presence: true
     validates! :description, length: { minimum: 20} ,presence: true
-    validates! :teams ,presence: true , inclusion: { in: 0..9 }, numericality: true
+    validates! :teams ,presence: true
     validates! :manager ,presence: true
 
-    has_many :teams
-    has_one :employee
+    # has_many :teams
+    # has_one :employee
 end
