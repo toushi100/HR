69403995d6c616ed41a7ddae233144e1e1bfd590
toushi100 <toushi2009@gmail.com>
2022-02-06 14:34:56 +0200

    added show view and functionality to target

--- app/controllers/target_controller.rb
+++ app/controllers/target_controller.rb
@@ -10,6 +10,12 @@ class TargetController < ApplicationController
     p @target.title
   end
 
+
+  def new
+    @target = Target.new
+    #@team = Team.all
+  end 
+  
   
   def destroy
     @target = Target.find(params[:id])
@@ -17,5 +23,28 @@ class TargetController < ApplicationController
     redirect_to target_path
   end
 
+
+  def create  
+    @target = Target.new#(post_params)
+    p "---------------------------------CREATE------------------------------"
+    @target.title = params[:title]
+    @target.description = params[:description]
+    @target.start_date = params[:start_date]
+    @target.finish_date = params[:finish_date]
+    @target.team = params[:team]
+    @target.status = params[:status]
+    @target.save
+    p "---------------------------------CREATED------DONE------------------------"
+
+    respond_to do |format|
+      if @target.save
+        format.html { redirect_to target_path, notice: "user was successfully created." }
+        format.json { render :show, status: :created, location: @target }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @target.errors, status: :unprocessable_entity }
+      end
+    end
+  end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,34 @@ Rails.application.routes.draw do
   get '/division' ,to:"division#index"
   get '/team' ,to:"team#index"
   get '/target' ,to:"target#index"
+
   delete '/target/:id' ,to:"target#destroy"
   delete '/user/:id' ,to:"user#destroy"
   delete '/employee/:id' ,to:"employee#destroy"
+  delete '/team/:id' ,to:"team#destroy"
+  delete '/division/:id' ,to:"division#destroy"
+
+
   get 'user/new', to: 'user#new', as: 'new_user' # new
   get 'employee/new', to: 'employee#new' # new
-  get 'devision/new', to: 'division#new' # new
-  get 'teams/new', to: 'team#new' # new
+  get 'division/new', to: 'division#new' # new
+  get 'team/new', to: 'team#new' # new
+  get 'target/new', to: 'target#new' # new
 
 
   post 'user/create', to: 'user#create' # new
   post 'employee/create', to: 'employee#create' # new
-  post 'devision/create', to: 'devision#create' # new
-  post 'teams/create', to: 'teams#create' # new
+  post 'division/create', to: 'division#create' # new
+  post 'team/create', to: 'team#create' # new
+  post 'target/create', to: 'target#create' # new
+
+
+  get 'user/:id' ,to:"user#show"
+  get 'employee/:id' ,to:"employee#show"
+  get 'division/:id' ,to:"division#show"
+  get 'target/:id' ,to:"target#show"
+  get 'team/:id' ,to:"team#show"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
