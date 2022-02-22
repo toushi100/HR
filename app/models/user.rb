811835d12f1a32fc486ea481843c6e1da90492e6
toushi100 <toushi2009@gmail.com>
2022-02-22 09:50:24 +0200

    adding devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-    validates! :name, length: { maximum: 15} ,presence: true
-    validates! :email,:uniqueness => {:case_sensitive => false} ,presence: true
-    validates! :passward,presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    # validates! :name, length: { maximum: 15} ,presence: true
+    # validates! :email,:uniqueness => {:case_sensitive => false} ,presence: true
+    # validates! :passward,presence: true
 
 
 end
