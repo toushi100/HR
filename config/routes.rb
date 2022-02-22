4150838e084fe136c7e333c4462f3f9f726bdde2
toushi100 <toushi2009@gmail.com>
2022-02-22 09:53:04 +0200

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
 
   get '/user' ,to:"user#index"
   get '/employee' ,to:"employee#index"
