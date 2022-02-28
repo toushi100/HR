86f624530d50beacda30e6655ce8dd4c3b92caf0
Ahmed Salah <toushi2009@gmail.com>
2022-02-28 17:29:56 +0200

    added config of i18n

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module HR
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.available_locales = [:en, :ar]
+    config.i18n.default_locale = :ar
 
     # Configuration for the application, engines, and railties goes here.
     #
